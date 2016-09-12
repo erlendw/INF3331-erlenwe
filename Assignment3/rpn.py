4611db567adfab4ac7648d98ee9c747ec5f4cee3
erlendw <erlendwestbye@gmail.com>
2016-09-12 15:30:55 +0200

    rpn stack

--- Assignment3/rpn.py
+++ Assignment3/rpn.py
@@ -1,8 +1,10 @@
-
+from math import sin
+from math import cos
+from math import sqrt
 theStack = []
-
-print("Numbers are sperated by spaces, you can input +, * and / to pop the two last numbers and calculate according to"
-      " your mathematical operation")
+possibleArguments = ['+','*','/','P','p','sin','cos','v']
+print("Numbers are sperated by spaces, you can input +, *, /, v, cos and sin to pop the two last numbers and calculate according to"
+      " your mathematical operation (v==square root)")
 
 while True:
 
@@ -10,6 +12,9 @@ while True:
     theInput = input()
     theInputArray = theInput.split()
 
+    print(theInputArray)
+
+    #adds integers to stack
     for i in range(len(theInputArray)):
         try:
             theResult=int(theInputArray[i])
@@ -17,26 +22,52 @@ while True:
         except:
             pass
 
-    if(theInput == "+" or theInput == "*" or theInput == "/"):
+    for i in range(len(theInputArray)):
         if(len(theStack) >= 2):
+            if(theInputArray[i] in possibleArguments):
 
-            x = theStack.pop(len(theStack)-1)
-            y = int(theStack.pop(len(theStack)-1))
+                x = theStack.pop(len(theStack)-1)
+                y = int(theStack.pop(len(theStack)-1))
 
-            if(theInput[0] == "+"):
-                res=(x+y)
-                print(str(x) + "+" + str(y) + "=" + str(res))
+                if(theInputArray[i] == "+"):
+                    res=(x+y)
+                    print(str(x) + "+" + str(y) + "=" + str(res))
+                    theStack.append(res)
 
-            if(theInput[0] == "*"):
-                res=(x*y)
-                print(str(x) + "*" + str(y) + "=" + str(res))
+                if(theInputArray[i] == "*"):
+                    res=(x*y)
+                    print(str(x) + "*" + str(y) + "=" + str(res))
+                    theStack.append(res)
 
-            if(theInput[0] == "/"):
-                res=(float(x)/float(y))
-                print(str(x) + "/" + str(y) + "=" + str(res))
+                if(theInputArray[i] == "/"):
+                    res=(float(x)/float(y))
+                    print(str(x) + "/" + str(y) + "=" + str(res))
+                    theStack.append(res)
+
+                if(theInputArray[i] == "P" or theInputArray[i] == "p"):
+                    print(theStack[len(theStack)-1])
+
+                if(theInputArray[i] == "sin"):
+                    res=sin(theStack[len(theStack)-1])
+                    print("sine of " + str(theStack[len(theStack)-1]) + " is " + str(res))
+                    theStack.append(res)
+
+                if(theInputArray[i] == "cos"):
+                    res=cos(theStack[len(theStack)-1])
+                    print("cosine of " + str(theStack[len(theStack)-1]) + " is " + str(res))
+                    theStack.append(res)
+
+                if(theInputArray[i] == "v"):
+                    res=sqrt(theStack[len(theStack)-1])
+                    print("Square root of " + str(theStack[len(theStack)-1]) + " is " + str(res))
+                    theStack.append(res)
+
 
         else:
             print("not eneough numbers in stack, you need at least 2")
+            break
+
+
 
     print("Current stack:")
     print(theStack)
