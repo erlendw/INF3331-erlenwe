553cdf29ce798757c8830fe6697a3e7813ce9041
erlendw <erlendwestbye@gmail.com>
2016-09-18 19:10:17 +0200

    get distributed

---
+++ Assignment3/my_unit_testing/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='my_unit_testing',
+      version='1337.0',
+      py_modules=['addition_testing','my_unit_testing'],
+      description='Skoleoppgave test som feiler',
+      author='Erlend Westbye',
+      author_email='erlendwestbye@gmail.com',
+      url='https://github.com/erlendw',
+      )
