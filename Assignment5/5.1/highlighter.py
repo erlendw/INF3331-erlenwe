a6fe690690e4bf6dc1e43a4e22856d1549089c15
erlendw <erlendwestbye@gmail.com>
2016-11-04 14:19:26 +0100

    oppg1 done and done sort of

---
+++ Assignment5/5.1/highlighter.py
@@ -0,0 +1,36 @@
+import sys
+import json
+import re
+from colors import color
+
+syspath = sys.path[0] + "/"
+
+args = sys.argv
+
+args.pop(0)
+
+sytaxfile_in = syspath + args[0] + ".json"
+themefile_in = syspath + args[1] + ".json"
+highlightfile_in = syspath + args[2]
+
+
+
+with open(sytaxfile_in) as data:
+    syntax = json.load(data)
+
+with open(sytaxfile_in) as data:
+    theme = json.load(data)
+
+with open(highlightfile_in, 'r') as data:
+    highlight = data.read()
+    #highlight = data.readlines()
+new = highlight
+for key in syntax:
+    pattern = (syntax[key])
+    match = re.findall(pattern , highlight)
+
+    for i in match:
+        replaced = (new.replace(i, color(i, fg=30)))
+        new = replaced
+
+print( "\n Colored verison: \n\n" + new)
