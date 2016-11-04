d317eec20d3dc948cc747f55e8fb5d7a405a850b
erlendw <erlendwestbye@gmail.com>
2016-11-04 14:31:16 +0100

    oppg1 done and done sort of

--- Assignment5/5.1/highlighter.py
+++ Assignment5/5.1/highlighter.py
@@ -9,28 +9,32 @@ args = sys.argv
 
 args.pop(0)
 
-sytaxfile_in = syspath + args[0] + ".json"
+syntaxfile_in = syspath + args[0] + ".json"
 themefile_in = syspath + args[1] + ".json"
 highlightfile_in = syspath + args[2]
 
 
-
-with open(sytaxfile_in) as data:
+with open(syntaxfile_in) as data:
     syntax = json.load(data)
 
-with open(sytaxfile_in) as data:
+with open(themefile_in) as data:
     theme = json.load(data)
 
 with open(highlightfile_in, 'r') as data:
     highlight = data.read()
     #highlight = data.readlines()
+
+
 new = highlight
 for key in syntax:
+
     pattern = (syntax[key])
+    color_ = int(theme[key])
+
     match = re.findall(pattern , highlight)
 
     for i in match:
-        replaced = (new.replace(i, color(i, fg=30)))
+        replaced = (new.replace(i, color(i, fg=color_)))
         new = replaced
 
 print( "\n Colored verison: \n\n" + new)
